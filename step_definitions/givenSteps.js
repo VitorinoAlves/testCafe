daf4d7460241252b255761f6469b7960f37a3134
Carlos Alexandre Vitorino Alves <carlos.alves@dextra-sw.com>
2021-07-22 17:33:06 -0300

    Configuring Config and Faker

--- step_definitions/givenSteps.js
+++ step_definitions/givenSteps.js
@@ -1,4 +1,5 @@
 const { I } = inject();
+const config = require('config');
 
 Given('a user on the home page', () => {
     I.amOnPage('/');
--- step_definitions/thenSteps.js
+++ step_definitions/thenSteps.js
@@ -1,5 +1,7 @@
 const { I } = inject();
+const faker = require('faker');
 
 Then('the user views the Teste page', () => {
     I.see('Seja bem vindo ao nosso teste!');
+    I.say(faker.name.findName());
 });
